1b91ea746b12e873b8d2404f289d77bcc8e48d8f
Rich Healey <richard.healey@99designs.com>
2012-08-14 18:00:37 +1000

    Fix windows newlines

--- test/test_locale.rb
+++ test/test_locale.rb
@@ -5,7 +5,7 @@ class TestLocale < Test::Unit::TestCase
   def setup
     ICU::Locale.default = 'en_US'
   end
-  
+
   def test_default
   	ICU::Locale.default = 'es_ES'
   	assert_equal('es_ES', ICU::Locale.default.locale_id)
@@ -13,22 +13,22 @@ class TestLocale < Test::Unit::TestCase
   	assert_equal('en_US', ICU::Locale.default.locale_id)
   	assert_raise(ArgumentError) { ICU::Locale.default = 'worg locale' }
   end
-  
+
   def test_get
   	assert_equal(ICU::Locale.get('es_ES').object_id, ICU::Locale.get('es_ES').object_id)
   	assert_equal(ICU::Locale.get('en_US').object_id, ICU::Locale.get('en_US').object_id)
   	assert_raise(ArgumentError) { ICU::Locale.get('worg locale') }
   end
-  
+
   def test_parent
   	locale = ICU::Locale.get('es_ES').parent
   	assert_equal('es', locale.locale_id)
   	locale = locale.parent
   	assert_equal(nil, locale)
-  	
+
   	assert_equal(nil, ICU::Locale.get('es_ES').parent.parent)
   end
-  
+
   def test_country
   	country = ICU::Locale.get('es_ES').country
   	assert_equal(ICU::Locale::Country.get('ES').object_id, country.object_id)
@@ -39,7 +39,7 @@ class TestLocale < Test::Unit::TestCase
   	assert_nil(ICU::Locale.get('es').country)
   	assert_raise(ArgumentError) { ICU::Locale::Country.get('worg country') }
   end
-  
+
   def test_language
   	language = ICU::Locale.get('es_ES').language
   	assert_equal(ICU::Locale::Language.get('es').object_id, language.object_id)
@@ -50,12 +50,12 @@ class TestLocale < Test::Unit::TestCase
   	assert_nil(ICU::Locale.get('_ES').language)
   	assert_raise(ArgumentError) { ICU::Locale::Language.get('worg language') }
   end
-  
+
   def test_available
   	assert_equal(["Afrikaans", "af"], ICU::Locale.available[0])
   	assert_equal(["afrikaans (Afrique du Sud)", "af_ZA"], ICU::Locale.available(:locale => 'fr_FR')[1])
   	assert_equal(["albanski (Albanija)", "sq_AL"], ICU::Locale.available(:locale => ICU::Locale.get('hr_HR'))[3])
-  	
+
   	assert_equal([["English (United States)", "en_US"], ["Spanish (Spain)", "es_ES"]], ICU::Locale.available(:allowed => ['es_ES', 'en_US']))
   	assert_equal(["English", "en"], ICU::Locale.available(:languages => ['es', 'en'])[0])
   	assert_equal(["Basque (Spain)", "eu_ES"], ICU::Locale.available(:countries => ['ES', 'US'])[0])
@@ -66,7 +66,7 @@ class TestLocale < Test::Unit::TestCase
 
   	assert_equal(["Albanian", [["Albanian", "sq"], ["Albanian (Albania)", "sq_AL"]]], ICU::Locale.available(:group => true)[1])
   end
-  
+
   def test_determine
   	assert_equal('es_ES', ICU::Locale.determine_from_http('es_ES,us_US'))
   end
--- test/test_time.rb
+++ test/test_time.rb
@@ -5,14 +5,14 @@ class TestTime < Test::Unit::TestCase
   def setup
     ICU::Locale.default = 'en_US'
   end
-  
+
   def test_localize_zone
   	Time.default_zone = 'Europe/Paris'
   	assert_equal('10:31:34 AM CEDT', Time.utc(2006, 5, 8, 8, 31, 34).localize(:date => false, :time => :full))
   	assert_equal('8:31:34 AM GMT+00:00', Time.utc(2006, 5, 8, 8, 31, 34).localize(:date => false, :time => :full, :zone => 'GMT'))
   	assert_equal('3:31:34 AM GMT-05:00', Time.utc(2006, 5, 8, 8, 31, 34).localize(:date => false, :time => :full, :zone => 'GMT-05:00'))
   end
-   
+
   def test_localize_style
   	assert_equal('Monday, May 8, 2006 8:31:34 AM', Time.utc(2006, 5, 8, 8, 31, 34).localize(:date => :full))
   	assert_equal('May 8, 2006 8:31:34 AM', Time.utc(2006, 5, 8, 8, 31, 34).localize(:date => :long))
@@ -25,13 +25,13 @@ class TestTime < Test::Unit::TestCase
   	assert_equal('May 8, 2006 8:31 AM', Time.utc(2006, 5, 8, 8, 31, 34).localize(:time => :short))
   	assert_equal('May 8, 2006', Time.utc(2006, 5, 8, 8, 31, 34).localize(:time => false))
   end
-  
+
   def test_localize_locale
   	assert_equal('lunes 8 de mayo de 2006 8:31:34', Time.utc(2006, 5, 8, 8, 31, 34).localize(:date => :full, :locale => 'es_ES'))
   	assert_equal('08:31:34 8 מאי 2006', Time.utc(2006, 5, 8, 8, 31, 34).localize(:date => :long, :locale => ICU::Locale.get('he_IL')))
   	assert_equal('2006. svibnja 08 08:31:34', Time.utc(2006, 5, 8, 8, 31, 34).localize(:date => :long, :locale => 'hr_HR'))
   end
-  
+
   def test_zones
 	assert_instance_of(Array, Time.zones)
 	assert_equal(["Africa/Ceuta", "Atlantic/Canary", "Europe/Madrid"], Time.zones(:countries => ['ES']))
@@ -41,7 +41,7 @@ class TestTime < Test::Unit::TestCase
   	)
   	assert_equal([["Spain", ["Africa/Ceuta", "Atlantic/Canary", "Europe/Madrid"]]], Time.zones(:countries => ['ES'], :group => true))
   end
-  
+
   def test_default_zone
   	Time.default_zone = 'Europe/Paris'
   	assert_equal('Europe/Paris', Time.default_zone)
@@ -53,7 +53,7 @@ class TestTime < Test::Unit::TestCase
   	assert_equal('GMT', Time.default_zone)
   	assert_equal('8:31:34 AM GMT+00:00', Time.utc(2006, 5, 8, 8, 31, 34).localize(:date => false, :time => :full))
   end
-  
+
   def test_pattern
   	assert_equal("EEEE d' de 'MMMM' de 'yyyy H:mm:ss", Time.pattern(:date => :full, :locale => 'es_ES'))
   	assert_equal('yyyy. MMMM dd HH:mm:ss', Time.pattern(:date => :long, :locale => ICU::Locale.get('hr_HR')))
@@ -66,7 +66,7 @@ class TestTime < Test::Unit::TestCase
   	assert_equal('MMM d, yyyy h:mm a', Time.pattern(:time => :short))
   	assert_equal('MMM d, yyyy', Time.pattern(:time => false))
   end
-  
+
   def test_week_start
   	assert_equal(1, Time.week_start)
   	assert_equal(2, Time.week_start(:locale => 'hr_HR'))
